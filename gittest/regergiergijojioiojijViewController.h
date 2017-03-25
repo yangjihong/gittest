abb9d2e2d7030878b1b272d63757da937c6b89f6
yangjihong <yjh@jideiMac.local>
2017-03-25 10:12:49 +0800

    nihaioo.
    efqefqewfeqwfeqfweqfqewqf

---
+++ gittest/regergiergijojioiojijViewController.h
@@ -0,0 +1,13 @@
+//
+//  regergiergijojioiojijViewController.h
+//  gittest
+//
+//  Created by yang on 2017/3/25.
+//  Copyright © 2017年 wuxihemei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface regergiergijojioiojijViewController : UIViewController
+
+@end
---
+++ gittest/regergiergijojioiojijViewController.m
@@ -0,0 +1,37 @@
+//
+//  regergiergijojioiojijViewController.m
+//  gittest
+//
+//  Created by yang on 2017/3/25.
+//  Copyright © 2017年 wuxihemei. All rights reserved.
+//
+
+#import "regergiergijojioiojijViewController.h"
+
+@interface regergiergijojioiojijViewController ()
+
+@end
+
+@implementation regergiergijojioiojijViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
